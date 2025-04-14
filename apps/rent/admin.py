177b7b4e3a7cc30874afdcfc13ccb26ccca3bce3
saa81 <saadatakmatova122@gmail.com>
2025-04-14 14:50:48 +0300

    Добавила HTML-шаблон и обновила представление

--- apps/rent/admin.py
+++ apps/rent/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from apps.rent.models import *
+
+# admin.site.register(Author)
+admin.site.register(Apartment)
+
+@admin.register(Landlord)
+class LandlordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'surname', 'album_count')
+    list_display_links = ('name', 'album_count')
+    list_filter = ('name', 'surname', 'album_count')
+    search_fields = ('name', 'surname')
+    ordering = ('id', 'name', 'surname')
--- apps/rent/models.py
+++ apps/rent/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Landlord(models.Model):
+    name = models.CharField(
+        max_length=50
+    )
+    surname = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+
+    )
+    album_count= models.PositiveIntegerField(
+        default=0,
+    )
+    image = models.PositiveIntegerField(
+        upload_to='landlords',
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+    class Meta:
+        verbose_name= 'Landlord'
+        verbose_name_plural = 'Landlords'
+
+class Apartment(models.Model):
+        landlord = models.ForeingKey(Landlord, on_delete=models.CASCADE)
+        name = models.CharField(max_length=250)
+        apartment_image = models.ImageField(upload_to='apartments/image')
+        file = models.FileField(upload_to='apartments')
+        created_at= models.DateTimeField(auto_now_add=True)
+        updated_at= models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+            return self.name
+
+
+        class Meta:
+            verbose_name = 'Apartment'
+            verbose_name_plural= 'Apartments'
